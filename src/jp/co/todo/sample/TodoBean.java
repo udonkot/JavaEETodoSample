e33072a7ed27dc62037c7b4a2f60d3eff48244cf
udonkot <udonko104@gmail.com>
2019-01-29 15:52:15 +0900

    コメント追加

--- src/jp/co/todo/sample/TodoBean.java
+++ src/jp/co/todo/sample/TodoBean.java
@@ -4,15 +4,38 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+/**
+ * TodoBeanクラス。
+ *
+ * @Namedアノテーションを付けることで、CDI管理BeanとなりJavaEE上でDIされます。
+ * 使用範囲に応じたスコープのアノテーションを付けることでBeanのライフサイクルが決まります。
+ *
+ * @RequestScoded
+ * @ApplicationScoped
+ * @SessionScoped
+ *
+ */
 @Named
 @RequestScoped
 public class TodoBean {
 
-	private String title = null;
-	private String content = null;
+	/** No */
 	private int no = 0;
+
+	/** タイトル */
+	private String title = null;
+
+	/** 内容 */
+	private String content = null;
+
+	/** 完了有無 */
 	private boolean finished = false;
 
+	/**
+	 * コンストラクタ
+	 * @PostCOnstructアノテーションを付けることでCDI初期化時(Bean生成後)に
+	 * 呼ばれます
+	 */
 	@PostConstruct
 	public void postConstruct() {
 		System.out.println("create:" + this.getTitle() + "/" + this.getContent() );
--- src/jp/co/todo/sample/TodoService.java
+++ src/jp/co/todo/sample/TodoService.java
@@ -8,14 +8,29 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+/**
+ * Todoサービスクラス
+ *
+ * Todoの登録、完了、削除など管理を行うクラス
+ * @ApplicationScopedを付けているため、サーバ起動時にインスタンスが生成されます。
+ *
+ */
 @Named
 @ApplicationScoped
 public class TodoService {
 
+	/**
+	 * @Injectアノテーションを付けることでDIされます。
+	 * new()をしなくともインスタンスが生成されます。
+	 */
 	@Inject
 	TodoBean todoBean;
 
+	/** TodoNo */
 	private int no = 0;
+
+	/** Todoリスト */
+	List<TodoBean> todoList = new ArrayList<>();
 
 	public int getNo() {
 		return no;
@@ -24,12 +39,6 @@ public class TodoService {
 	public void setNo(int no) {
 		this.no = no;
 	}
-
-	private void countUp() {
-		this.no++;
-	}
-
-	List<TodoBean> todoList = new ArrayList<>();
 
 	public TodoBean getTodoBean() {
 		return todoBean;
@@ -47,48 +56,87 @@ public class TodoService {
 		this.todoList = todoList;
 	}
 
+	/**
+	 * TodoNo採番
+	 */
+	private void countUp() {
+		this.no++;
+	}
+
+	/**
+	 * Todo登録処理
+	 * @return 画面名
+	 */
 	public String regist() {
+		// No更新
 		this.countUp();
+
+		// 新規Todoオブジェクト生成
 		TodoBean newTodo = new TodoBean();
 		newTodo.setContent(this.getTodoBean().getContent());
 		newTodo.setTitle(this.getTodoBean().getTitle());
 		newTodo.setNo(this.getNo());
 		newTodo.setFinished(false);
+
+		// Todoリストに追加
 		this.getTodoList().add(newTodo);
 
 		return "index";
 	}
 
+	/**
+	 * Todo更新処理
+	 * @param finishNo 完了No
+	 * @return 画面名
+	 */
 	public String finish(int finishNo) {
-		System.out.println(finishNo + "click");
-		List<TodoBean> newTodoList = this.getTodoList().stream()
-		.map(todo -> checkFinish(todo, finishNo))
-		.collect(Collectors.toList());
 
+		// for Debug
+		System.out.println(finishNo + "click");
+
+		// Noが一致するTodoをチェック
+		List<TodoBean> newTodoList = this.getTodoList().stream()
+				.map(todo -> checkFinish(todo, finishNo))
+				.collect(Collectors.toList());
+
+		// 更新後のTodoリストセット
 		this.setTodoList(newTodoList);
 
 		return "index";
 	}
 
+	/**
+	 * Todo削除処理
+	 * @param deleteNo 削除No
+	 * @return 画面名
+	 */
 	public String delete(int deleteNo) {
 
+		// filterで削除対象のTodoをリストから除く
 		List<TodoBean> newTodoList = this.getTodoList().stream()
-		.filter(todo -> !((todo.getNo() == deleteNo) && todo.isFinished()))
-		.collect(Collectors.toList());
+				.filter(todo -> !((todo.getNo() == deleteNo) && todo.isFinished()))
+				.collect(Collectors.toList());
 
+		// 削除後のTodoリストセット
 		this.setTodoList(newTodoList);
 
 		return "index";
 	}
 
+	/**
+	 * Todo完了チェック
+	 * @param todo Todo
+	 * @param finishNo 完了No
+	 * @return
+	 */
 	private TodoBean checkFinish(TodoBean todo, int finishNo) {
-		if(todo.getNo() == finishNo) {
+		// Noが一致する場合に完了とする。
+		if (todo.getNo() == finishNo) {
+			// for debug
 			System.out.println(todo.getNo() + "一致！");
 			todo.setFinished(true);
 		}
 		return todo;
 	}
-
-
 
 }
